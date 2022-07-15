76f2e082e3cba33b133268db26273b049a3c06bd
aabb1998 <53510613+aabb1998@users.noreply.github.com>
2022-07-15 18:41:53 +1000

    first commmit

---
+++ server/db.js
@@ -0,0 +1,15 @@
+const { Mongoose, default: mongoose } = require('mongoose');
+
+module.exports = () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  try {
+    mongoose.connect(process.env.DB, connectionParams);
+    console.log('Connected to database successfully...');
+  } catch (error) {
+    console.log(error);
+    console.log("Couldn't connect to database");
+  }
+};
