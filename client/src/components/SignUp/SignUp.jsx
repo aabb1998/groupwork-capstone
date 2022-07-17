dd7e80db8b248aaa47bc31fc32699d78ea382b49
aabb1998 <53510613+aabb1998@users.noreply.github.com>
2022-07-17 19:11:03 +1000

    New packages

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import FormData from 'form-data';
 
 const SignUp = () => {
   const [data, setData] = useState({
@@ -16,13 +17,23 @@ const SignUp = () => {
     setData({ ...data, [input.name]: input.value });
   };
 
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const formData = new FormData();
+    Object.keys(data).forEach((key) => {
+      formData.append(key, data[key]);
+    });
     try {
-      const url = 'http://localhost:3000/api/users';
-      const { data: res } = await axios.post(url, 'hh');
-      navigate('/login');
-      console.log(res.message);
+      let res = await axios.post('http://localhost:3000/api/users', formData, {
+        headers: form_data.getHeaders(),
+      });
+
+      let response = res.data;
+      console.log(response);
     } catch (error) {
       if (
         error.response &&
